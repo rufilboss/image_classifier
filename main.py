3400b9f677e852e15f782c10498f56b8605005bb
Ilyas Rufai <rufaiilyas@gmail.com>
2024-09-15 21:59:17 +0100

    gen -f image

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 from tensorflow.keras import datasets, layers, models
 
+matplotlib.use('Agg')
 
+# Load CIFAR-10 dataset
 (training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
-training_images, testing_images = training_images / 255, testing_images / 255
 
-class_name = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+# Normalize images
+training_images, testing_images = training_images / 255.0, testing_images / 255.0
+
+# Class names for the CIFAR-10 dataset
+class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+
+# Plot the first 16 images
+plt.figure(figsize=(10, 10))  # larger figure for better visibility
 
 for i in range(16):
     plt.subplot(4, 4, i+1)
-    plt.imshow(training_images[i], cmap=plt.cm.binary)
-    plt.xticks([])
-    plt.yticks([])
-    plt.title(class_name[training_labels[i][0]])
-    
+    plt.imshow(training_images[i])  # , cmap=plt.cm.binary | Add cmap for better contrast, remove it if not needed
+    plt.xticks([])  # Remove x-axis ticks
+    plt.yticks([])  # Remove y-axis ticks
+    plt.title(class_names[training_labels[i][0]])
+
+plt.tight_layout()  # Prevent overlap of subplots
 plt.show()
+
+# plt.savefig('output_image.png')
