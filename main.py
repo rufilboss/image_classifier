8f9db1894c551f864bcbcc5c437a7e3e99cd64eb
Ilyas Rufai <rufaiilyas@gmail.com>
2024-09-16 11:34:13 +0100

    update

--- main.py
+++ main.py
@@ -29,3 +29,35 @@ plt.tight_layout()  # Prevent overlap of subplots
 plt.show()
 
 # plt.savefig('output_image.png')
+
+# TRAINING THE MODEL
+
+# 
+training_images = training_images[:20000]
+training_labels = training_labels[:20000]
+testing_images = testing_images[:4000]
+testing_labels = testing_labels[:4000]
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+# Compling
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# Training
+model.fit(training_images, training_labels, epochs=10, validation_data=(testing_images, testing_labels))
+
+# Evaluate the model
+loss, accuracy = model.evaluate(testing_images, testing_labels)
+print(f"Loss: {loss}")
+print(f"Accuracy: {accuracy}")
+
+# Save the model
+model.save('image_classifier.keras')
